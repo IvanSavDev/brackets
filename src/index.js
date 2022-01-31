19643f502fcc6eb5911d8603a721a472e5c14623
Ivan <mortlilport@mail.ru>
2022-01-31 16:49:43 +0300

    solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  const obj = {};
+  const keyObj = [];
+  const sameValue = [];
+
+  for (const element of bracketsConfig) {
+    obj[element[1]] = element[0];
+    keyObj.push(element[0]);
+    if(element[0] === element[1]) {
+      sameValue.push(element[0]);
+    }
+  }
+
+  for(let i = 0; i < str.length; i++) {
+    if(keyObj.includes(str[i])) {
+
+      if(str[i] === stack[stack.length - 1] && sameValue.includes(str[i])) {
+        stack.pop();
+        continue;
+      }
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (stack[stack.length - 1] === obj[str[i]]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
